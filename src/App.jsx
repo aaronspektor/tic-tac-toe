d68b5a60bf3d22283211607fe126a0cfa1870f45
Aaron Spektor <a.m.spektor@wustl.edu>
2025-04-01 22:48:16 -0500

    Added handleClick functionality for a couple of squares, rather than just top-left (just to make sure it works)

--- src/App.jsx
+++ src/App.jsx
@@ -11,18 +11,18 @@ function Square({ value, onSquareClick }) {
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   
-  function handleClick() {
+  function handleClick(i) {
     const nextSquares = squares.slice();
-    nextSquares[0] = "X";
+    nextSquares[i] = "X";
     setSquares(nextSquares);
   }
   
   return (
     <>
     <div className="board-row">
-    <Square value={squares[0]} onSquareClick={handleClick} />
-    <Square value={squares[1]} onSquareClick={handleClick}/>
-    <Square value={squares[2]} onSquareClick={handleClick}/>
+    <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+    <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+    <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
     </div>
     <div className="board-row">
     <Square value={squares[3]} onSquareClick={handleClick}/>
